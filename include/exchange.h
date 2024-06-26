50c1ba83867de030ba80b503945f6b12ddec395a
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-06-26 17:00:04 -0400

    working on object exchange

---
+++ include/exchange.h
@@ -0,0 +1,20 @@
+#ifndef EXCHANGE_H
+#define EXCHANGE_H
+
+#include "common.h"
+
+
+typedef struct exchange_bid {
+
+} Exchange_Bid;
+
+
+
+typedef struct exchange_offer {
+
+} Exchange_Offer;
+
+
+
+
+#endif
