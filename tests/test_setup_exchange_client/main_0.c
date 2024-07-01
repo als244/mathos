10c7809c7b09a90cf3a18314324b35cdeda96e5c
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-06-30 21:40:04 -0400

    updating limits on max QP work requests / channel capacities

--- tests/test_setup_exchange_client/main_0.c
+++ tests/test_setup_exchange_client/main_0.c
@@ -41,15 +41,30 @@ int main(int argc, char * argv[]){
 	// 2.) Initialize Own Exchanges_Client
 	printf("Initializing Exchanges Client...\n\n");
 	uint64_t max_exchanges = 1;
-	uint64_t max_outstanding_bids = 1UL << 8;
+	uint64_t max_outstanding_bids = 1UL << 12;
 	Exchanges_Client * exchanges_client = init_exchanges_client(max_exchanges, max_outstanding_bids);
 	if (exchanges_client == NULL){
 		fprintf(stderr, "Error: could not initialize exchanges client\n");
 		return -1;
 	}
 
+	// Maximum number of 2^15 work requests for each send/recv side of queue pair
+	//	- need not set to be symmetric. can also use SRQ which has limit of 2^15 - 1
 
-	uint16_t capacity_channels = 1 << 8;
+
+	// For now: same QP used for 3 recv channels and and 1 send channel in "setup_exchange_connection"
+	//		- per connection! (but connections can be sharded across the UD QPs)
+	// For now: same QP used fdor 3 send chanels and 1 recv channel in "setup_client_connection"
+	//		- per connection! (but connections can sharded across the UD QPs)
+
+	// THUS: for completley symmetric channel capacity must be <= max_work_requests_per_qp_per_side / (max_per_side_channels_per_connection * num_connections_per_qp)
+	//														   <= 2^15 / (3 * 1) <= 2^13, but this is excessive...
+	//	- for now doing it this way because simpler to configure...
+
+	//	- IMPORTANT: need not be symmetric across different types of channels (message types, recv vs. send)
+	//					- in reality we want the number of recv work requests for outstanding bids to be the highest by far!
+
+	uint16_t capacity_channels = 1U << 12;
 
 	// 3.) Setup connection to other exchanges
 	printf("Setting up connection to exchange: %lu\n\n", OTHER_EXCH_ID);
