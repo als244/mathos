d7a856ade17afe117e2d6dd56613d0e2c7d75c66
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-07-21 15:58:11 -0400

    trying without create flags

--- tests/test_qp/setup_net.c
+++ tests/test_qp/setup_net.c
@@ -135,7 +135,7 @@ QP * init_qp(QueuePairUsageType qp_usage_type, struct ibv_context * ibv_dev_ctx,
 
 	qp_attr.comp_mask = IBV_QP_INIT_ATTR_SEND_OPS_FLAGS | IBV_QP_INIT_ATTR_PD | IBV_QP_INIT_ATTR_CREATE_FLAGS;
 	qp_attr.source_qpn = source_qpn;
-	qp_attr.create_flags = IBV_QP_CREATE_SOURCE_QPN;
+	//qp_attr.create_flags = IBV_QP_CREATE_SOURCE_QPN;
 
 	struct ibv_qp * ibv_qp = ibv_create_qp_ex(ibv_dev_ctx, &qp_attr);
 	if (ibv_qp == NULL){
