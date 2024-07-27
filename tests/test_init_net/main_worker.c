f1ba45159831221f800a4ec3c89005b201eaeecb
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-07-26 20:59:45 -0400

    ensuring that worker's rdma_init stays active even after intialization. The network init seems good to go. Now need to work on data transfers and inventory testing

--- tests/test_init_net/main_worker.c
+++ tests/test_init_net/main_worker.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char * argv[]){
 
+	int ret;
+	
 	if ((argc != 2) && (argc != 3)){
 		fprintf(stderr, "Error: Usage ./testWorker <master_ip_addr> <self_ip_addr>\n");
 		return -1;
@@ -21,7 +23,18 @@ int main(int argc, char * argv[]){
 		return -1;
 	}
 
-	printf("\n\nSuccessfully initialized network!\n");
+	printf("\n\nSuccessfully initialized network!\n\n\n");
+
+
+	printf("Letting the rdma_init server stay running in case of more joins...\n");
+
+	// Should Be Infinitely Blocking 
+	// (unless error or shutdown message)
+	ret = pthread_join(net_world -> tcp_rdma_init_server_thread, NULL);
+	if (ret != 0){
+		fprintf(stderr, "Error: pthread_join failed for join server\n");
+		return -1;
+	}
 
 	return 0;
 }
