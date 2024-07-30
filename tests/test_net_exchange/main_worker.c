e5ad075e4be22da796c40093caa4ed46e960fae7
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-07-30 18:34:48 -0400

    testing where cq goes wrong

--- tests/test_net_exchange/main_worker.c
+++ tests/test_net_exchange/main_worker.c
@@ -40,7 +40,7 @@ int main(int argc, char * argv[]){
 		dest_node_id = 1;
 	}
 
-	uint64_t num_exchange_messages = 1 << 10;
+	uint64_t num_exchange_messages = 1 << 4;
 
 	// prepare all contorl messages
 
