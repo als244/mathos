2bc18a5c6f90b5f19fa46e79b770296625e29762
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-10-08 12:10:17 -0400

    Testing reservations on gpu mem

--- tests/test_inventory/main_worker_1.c
+++ tests/test_inventory/main_worker_1.c
@@ -53,11 +53,14 @@ int main(int argc, char * argv[]){
 	
 	// 2 MB Chunk Size
 	int device_id = 0;
-	uint64_t chunk_size = 1U << 16;
-	uint64_t num_chunks = 10000;
+	uint64_t device_chunk_size = 1UL << 16;
+
+	uint64_t GPU_MEM_SIZE = 20 * (1ULL << 30);
+	uint64_t GPU_MEM_FRAC = .25;
+	uint64_t device_num_chunks = (GPU_MEM_FRAC * GPU_MEM_SIZE) / device_chunk_size;
 
 	// should return a 2GB region
-	ret = hsa_add_device_memory(hsa_memory, device_id, num_chunks, chunk_size);
+	ret = hsa_add_device_memory(hsa_memory, device_id, device_num_chunks, device_chunk_size);
 	if (ret != 0){
 		fprintf(stderr, "Error: failed to add device memory\n");
 		return -1;
